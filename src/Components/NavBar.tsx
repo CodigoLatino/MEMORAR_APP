f7790edb5c1ec3ff6918560d206f22ad58ef087f
Lestradez <omar.lestrade@est.fi.uncoma.edu.ar>
2021-05-06 05:57:46 -0300

    Add hamburger event

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,13 @@
+import { createPublicKey } from "node:crypto";
 import { Link } from "wouter";
 export const NavBar = () => {
+  
+  const menu = document.querySelector('#menu');
+  const hamburger=()=>{
+  if (menu) { 
+      menu.classList.toggle('hidden');
+    }
+    };
   
   return (
     <>
@@ -10,11 +18,11 @@ export const NavBar = () => {
             <span className="font-semibold text-4xl tracking-tight">Memorar App</span>
           </div>
           <div className="block lg:hidden">
-            <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-typo-dark hover:border-typo-dark">
+            <button id="hamburger" onClick={hamburger} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-typo-dark hover:border-typo-dark">
               <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
           </div>
-          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div id="menu" className="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:text-right hidden">
             <div className="text-sm lg:flex-grow">
               <div>
                 <Link to="/" className="block font-semibold text-xl px-4 py-2 lg:pt-6 lg:pb-4 mt-4 lg:inline-block lg:mt-0 text-typo hover:text-typo-dark  hover:bg-hovernav mr-4">
